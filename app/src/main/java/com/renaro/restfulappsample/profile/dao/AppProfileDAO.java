74cdb76752f8bef527d5b7fb4cf6339dfd6bc39e
Renaro Paula Santos <renaropsantos@gmail.com>
2018-01-28 21:38:45 -0200

    removing unnecessary imports

--- app/src/main/java/com/renaro/restfulappsample/profile/dao/AppProfileDAO.java
+++ app/src/main/java/com/renaro/restfulappsample/profile/dao/AppProfileDAO.java
@@ -13,8 +13,6 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.OkHttpClient;
-import retrofit2.Call;
-import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
